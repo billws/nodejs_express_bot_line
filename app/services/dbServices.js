5be05dd9bce324ba8380dd389cc45b921365ae7f
Bill Wu <billwstw@gmail.com>
2017-12-14 02:37:40 +0800

    Test

--- app/services/dbServices.js
+++ app/services/dbServices.js
@@ -10,27 +10,12 @@ const dbServices = {
     init: function(){
         client.connect();
 
-        client.query(`CREATE TABLE players (
-            id UUID,
-            name VARCHAR(30),
-            lineid VARCHAR(100),
-            sendto VARCHAR(30)
-            );`, (err, res) => {
+        client.query('CREATE TABLE players (id UUID, name VARCHAR(30), lineid VARCHAR(100), sendto VARCHAR(30));', (err, res) => {
             if (err) throw err;
-            
             console.log(JSON.stringify(row));
-            
-            
         });
-        client.query(`INSERT INTO players (name) VALUES
-            ('小左'),
-            ('卡卡'),
-            ('書包'),
-            ('瑋誠'),
-            ('香香'),
-            ('旅人');`, (err, res) => {
+        client.query('INSERT INTO players (name) VALUES("小左"), ("卡卡"), ("書包"), ("瑋誠"), ("香香"), ("旅人");', (err, res) => {
             if (err) throw err;
-            
             console.log(JSON.stringify(row));
         });
 
