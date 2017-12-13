dae760488433cce9f1426ea6f98694f5d8a52b21
Bill Wu <billwstw@gmail.com>
2017-12-14 00:19:38 +0800

    test again.

--- app/app.js
+++ app/app.js
@@ -24,7 +24,7 @@ bot.on('message', function(event) {
         bot.push(userId, sendMsg);
         console.log('userID: ' + userId);
         console.log('send: ' + sendMsg);
-        }, 10000);
+      }, 1000);
     }
 });
 
