3ccf5dc2b20055a26302907bd3eaa2cd42db88b3
Bill Wu <billwstw@gmail.com>
2017-12-15 05:56:38 +0800

    Beta!

--- app/services/bot_rules.js
+++ app/services/bot_rules.js
@@ -56,12 +56,6 @@ const botRules = {
         return client.replyMessage(event.replyToken, echo);
     },
     Robot: function(client, event){
-        // create a echoing text message
-        /*const echo = [{ type: 'text', text: '我很笨還沒學會怎麼回覆你！！' }, 
-            {type: 'sticker', packageId: 3, stickerId: 188}];*/
-        
-        // use reply API
-        /*return client.replyMessage(event.replyToken, echo);*/
         apiaiServices.easyRequest(client, event);
     },
     InitDB: function(client, event){
