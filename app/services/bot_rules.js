5e5e7d7ffdc5a813b2f2141cc6d6ae39b6284c8d
Bill Wu <billwstw@gmail.com>
2017-12-15 01:20:31 +0800

    test

--- app/services/bot_rules.js
+++ app/services/bot_rules.js
@@ -25,7 +25,7 @@ const botRules = {
         let firstIndex = event.message.text.indexOf("我是");
         let lastIndex = event.message.text.indexOf(process.env.SS);
         if(firstIndex === 0 && lastIndex > 0){
-            return dbServices.AttendDrawing(client, event.replyToken, event.message.text.slice(firstIndex, lastIndex + 1), event.source.userId, 2017, 0);
+            return dbServices.AttendDrawing(client, event.replyToken, event.message.text.slice(firstIndex + 2, lastIndex), event.source.userId, 2017, 0);
         }else{
             const echo = { type: 'text', text: '格式錯誤啦！' };
             return client.replyMessage(event.replyToken, echo);
